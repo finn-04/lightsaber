27b8b6860307bbc3943d33da7658d5cf63d57069
Anna Briamonte <abriamon@nd.edu>
2024-11-02 15:15:20 -0400

    base color

--- leds/leds_wheel_presets.ino
+++ leds/leds_wheel_presets.ino
@@ -2,27 +2,23 @@
 # include <ArduinoBLE.h>
 
 # define LED_PIN    6
-# define LED_COUNT  20
-# define POT_PIN    A0
+# define LED_COUNT 20
 
 BLEService myService ("180F");
 BLECharacteristic myCharacteristic ("12345678-1234-5678-1234-56789abcdef0", BLERead | BLEWrite, 20);
 
-Adafruit_NeoPixel strip (LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+// storing received preset
 String value;
 uint32_t color = strip.Color (255, 0, 0);
-// track if color is controlled by Bluetooth or potentiometer
-bool control_ble = false;
 
-void setup ()
-{
+void setup() {
   Serial.begin (9600);
   if (!BLE.begin ())
   {
     Serial.println ("starting BLE failed");
     while (1);
   }
-
   BLE.setLocalName ("NanoBLE");
   BLE.setAdvertisedService (myService);
   myService.addCharacteristic (myCharacteristic);
@@ -30,12 +26,23 @@ void setup ()
   myCharacteristic.setValue ("0");
   BLE.advertise ();
   Serial.println ("BLE device active, waiting for connections...");
-  strip.begin ();
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // turn OFF all pixels ASAP
+  strip.setBrightness(255);
+
+  for (int i = 0; i < LED_COUNT; i++)
+  {
+    Serial.println (value);
+    strip.setPixelColor (i, color);
+  }
   strip.show ();
-  strip.setBrightness (255);
+
 }
 
-void loop ()
+
+// loop() function -- runs repeatedly as long as board is on ---------------
+
+void loop() 
 {
   BLEDevice central = BLE.central ();
 
@@ -56,12 +63,10 @@ void loop ()
         {
           buffer [length] = '\0';
         }
-
+        // update with received
         value = String ((char *) buffer);
         Serial.print ("received: ");
         Serial.println (value);
-
-        control_ble = true;
 
         if (value == "0")
         {
@@ -93,64 +98,17 @@ void loop ()
           color = strip.Color (255, 255, 255);
           Serial.println ("five");
         }
-
-        Serial.println ("color set by Bluetooth");
+        // update strip
+        for (int i = 0; i < LED_COUNT; i++)
+        {
+          Serial.print ("VALUE: ");
+          Serial.println (value);
+          strip.setPixelColor (i, color);
+        }
+      strip.show ();
       }
     }
-    Serial.print ("disconnected from central: ");
+    Serial.print ("disconected from central: ");
     Serial.println (central.address ());
-    control_ble = false;
   }
-  else if (!control_ble)
-  {
-    int pot_val = analogRead (POT_PIN);
-    int range = map (pot_val, 0, 1023, 0, 255);
-    int rgb_range = map (range, minimum, maximum, 0, 6);
-
-    int g_up = map (range, 0, 42, minimum, maximum);
-    int r_down = map (range, 43, 84, minimum, maximum);
-    int b_up = map (range, 85, 127, minimum, maximum);
-    int g_down = map (range, 128, 169, maximum, minimum);
-    int r_up = map (range, 170, 212, minimum, maximum);
-    int b_down = map (range, 213, 255, maximum, minimum);
-
-    switch (rgb_range)
-    {
-      case 0:
-      {
-        color = strip.Color (255, g_up, 0);
-        break;
-      }
-      case 1:
-      {
-        color = strip.Color (r_down, 255, 0);
-        break;
-      }
-      case 2:
-      {
-        color = strip.Color (0, 255, b_up);
-        break;
-      }
-      case 3:
-      {
-        color = strip.Color (0, g_down, 255);
-        break;
-      }
-      case 4:
-      {
-        color = strip.Color (r_up, 0, 255);
-        break;
-      }
-      case 5:
-      {
-        color = strip.Color (255, 0, b_down);
-        break;
-      }
-    }
-  }
-  for (int i = 0; i < LED_COUNT; i++)
-  {
-    strip.setPixelColor (i, color);
-  }
-  strip.show ();
 }
