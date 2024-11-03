bc1adfbae24afb51b9c30b044e474e4aafb875e1
Anna Briamonte <abriamon@nd.edu>
2024-11-03 11:16:42 -0500

    persisting preset

--- leds/on_off.ino
+++ leds/on_off.ino
@@ -33,6 +33,9 @@ String value;
 uint32_t color = strip.Color (255, 0, 0);
 // track if color is controlled by Bluetooth or potentiometer
 bool control_ble = false;
+// potentiomter control
+int last_pot_val = -1;
+const int pot_threshold = 20;
 
 void setup ()
 {
@@ -170,51 +173,55 @@ void loop ()
     else if (!control_ble)
     {
       int pot_val = analogRead (POT_PIN);
-
       Serial.print ("potientiometer value: ");
       Serial.println (pot_val);
-      int range = map (pot_val, 0, 1023, 0, 255);
-      int rgb_range = map (range, minimum, maximum, 0, 6);
-
-      int g_up = map (range, 0, 42, minimum, maximum);
-      int r_down = map (range, 43, 84, minimum, maximum);
-      int b_up = map (range, 85, 127, minimum, maximum);
-      int g_down = map (range, 128, 169, maximum, minimum);
-      int r_up = map (range, 170, 212, minimum, maximum);
-      int b_down = map (range, 213, 255, maximum, minimum);
-
-      switch (rgb_range)
+      // check if difference from last value is greater than threshold
+      if (last_pot_val == -1 || abs (pot_val - last_pot_val) > pot_threshold)
       {
-        case 0:
+        int range = map (pot_val, 0, 1023, 0, 255);
+        int rgb_range = map (range, minimum, maximum, 0, 6);
+
+        int g_up = map (range, 0, 42, minimum, maximum);
+        int r_down = map (range, 43, 84, minimum, maximum);
+        int b_up = map (range, 85, 127, minimum, maximum);
+        int g_down = map (range, 128, 169, maximum, minimum);
+        int r_up = map (range, 170, 212, minimum, maximum);
+        int b_down = map (range, 213, 255, maximum, minimum);
+
+        switch (rgb_range)
         {
-          color = strip.Color (255, g_up, 0);
-          break;
+          case 0:
+          {
+            color = strip.Color (255, g_up, 0);
+            break;
+          }
+          case 1:
+          {
+            color = strip.Color (r_down, 255, 0);
+            break;
+          }
+          case 2:
+          {
+            color = strip.Color (0, 255, b_up);
+            break;
+          }
+          case 3:
+          {
+            color = strip.Color (0, g_down, 255);
+            break;
+          }
+          case 4:
+          {
+            color = strip.Color (r_up, 0, 255);
+            break;
+          }
+          case 5:
+          {
+            color = strip.Color (255, 0, b_down);
+            break;
+          }
         }
-        case 1:
-        {
-          color = strip.Color (r_down, 255, 0);
-          break;
-        }
-        case 2:
-        {
-          color = strip.Color (0, 255, b_up);
-          break;
-        }
-        case 3:
-        {
-          color = strip.Color (0, g_down, 255);
-          break;
-        }
-        case 4:
-        {
-          color = strip.Color (r_up, 0, 255);
-          break;
-        }
-        case 5:
-        {
-          color = strip.Color (255, 0, b_down);
-          break;
-        }
+        last_pot_val = pot_val;
       }
     }
     for (int i = 0; i < LED_COUNT; i++)
